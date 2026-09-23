ff47bd57585b3e0f01181235b186341428de278c
Jdiego <jdmorenover@gmail.com>
2026-09-23 02:53:33 -0500

    Agrega Pregunta 4

---
+++ app/src/main/java/com/example/appgrupo12/Pregunta4Activity.kt
@@ -0,0 +1,60 @@
+package com.example.appgrupo12
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appgrupo12.databinding.ActivityPregunta4Binding
+
+class Pregunta4Activity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityPregunta4Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityPregunta4Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom
+            )
+            insets
+        }
+
+        binding.button.setOnClickListener(this)
+    }
+
+    fun calcularDatos() {
+
+        val datos = binding.txtgigas.text.toString().toDouble()
+
+        if (datos <= 30) {
+
+            binding.textView2.text =
+                "Consumo dentro de su plan contratado."
+
+        } else {
+
+            val exceso = datos - 30
+            val cobro = 25 + (exceso * 6)
+
+            val datosFormateados = String.format("%.2f", datos)
+            val excesoFormateado = String.format("%.2f", exceso)
+            val cobroFormateado = String.format("%.2f", cobro)
+
+            binding.textView2.text =
+                "Datos consumidos: $datosFormateados GB\n" +
+                        "Exceso de GB: $excesoFormateado GB\n" +
+                        "Cobro adicional generado: S/ $cobroFormateado"
+        }
+    }
+
+    override fun onClick(p0: View) {
+        calcularDatos()
+    }
+}
